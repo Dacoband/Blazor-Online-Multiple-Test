ed805531786ae23a55f967dbb8930811bdf6b8c8
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-12-05 22:36:36 +0700

    First Commit Sonic Choice

---
+++ SonicChoice/SonicChoice.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SonicChoice.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
